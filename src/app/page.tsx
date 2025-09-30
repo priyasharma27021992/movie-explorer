79b258d9127f712d2155a070c97f9b2fbf7a136b
Priya Sharma <priyasharma2702@gmail.com>
2025-09-30 23:28:38 +0200

    added first draft from middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default async function Home() {
 	const res = await fetch(
 		`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_KEY}`,
@@ -10,7 +12,10 @@ export default async function Home() {
 			<h1>Trending Movies(SSR)</h1>
 			<ul>
 				{data.results?.map((movie: { id: string; title: string }) => (
-					<li key={movie.id}>{movie.title}</li>
+					<li key={movie.id}>
+						{movie.title}
+						<Link href={`/movie/${movie.id}`}>Check More</Link>
+					</li>
 				))}
 			</ul>
 		</main>
